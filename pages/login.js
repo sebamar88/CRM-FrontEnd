ba6da89f18bfd55ecea0a6ae99ac4752ac928218
Sebastián Martinez <sebamar88@gmail.com>
2021-03-26 11:48:55 -0300

    Nueva Cuenta

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { gql, useMutation } from '@apollo/client';
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const AUTENTICAR_USUARIO = gql`
     mutation autenticarUsuario($input: AutenticarInput) {
@@ -153,6 +154,9 @@ const Login = () => {
 
                         </form>
                     </div>
+                    <Link href="/nuevacuenta">
+                        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Crear Cuenta</a>
+                    </Link>
                 </div>
             </Layout>
         </>
