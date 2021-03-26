9f3d66a93a79eda9dc19f72f8fd320f8822529cb
Sebastián Martinez <sebamar88@gmail.com>
2021-03-26 11:51:16 -0300

    Arreglo

--- pages/login.js
+++ pages/login.js
@@ -153,10 +153,11 @@ const Login = () => {
                             />
 
                         </form>
+                        <Link href="/nuevacuenta">
+                            <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Crear Cuenta</a>
+                        </Link>
                     </div>
-                    <Link href="/nuevacuenta">
-                        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Crear Cuenta</a>
-                    </Link>
+                    
                 </div>
             </Layout>
         </>
